f231375709dcc717e1dbe1b7dbcf5f9867d1c469
GustavoAlbieri <gustavo.albieri1999@gmail.com>
2023-07-25 21:17:11 -0300

    Seção 1 .NET 5

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public EventoController()
+        {
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get() 
+        {
+            return new Evento[] {
+                new Evento() {
+                    EventoID = 1,
+                    Local = "Irapuã",
+                    DataEvento = DateTime.Now.AddDays(2).ToString(),
+                    Tema = "Terror",
+                    NumeroPessoas = 25,
+                    Lote = "1 - Lote",
+                    ImagemURL = "Foto.png"
+                },
+                new Evento() {
+                    EventoID = 2,
+                    Local = "Sales",
+                    DataEvento = DateTime.Now.AddDays(2).ToString(),
+                    Tema = "Animation",
+                    NumeroPessoas = 264,
+                    Lote = "1 - Lote",
+                    ImagemURL = "Fotos.png"
+                },
+                new Evento() {
+                    EventoID = 3,
+                    Local = "Urupes",
+                    DataEvento = DateTime.Now.AddDays(2).ToString(),
+                    Tema = "Testeeer",
+                    NumeroPessoas = 64,
+                    Lote = "4 - Lote",
+                    ImagemURL = "Tester.png"
+                }
+            };
+        }
+
+        [HttpPost]
+        public string Post() => "EXEMPLO DE POST";
+    }
+}
---
+++ Back/src/ProEventos.API/Models/Evento.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProEventos.API.Models
+{
+    public class Evento
+    {
+        public int EventoID { get; set; }
+        public string Local { get; set; }
+        public string DataEvento { get; set; }
+        public string Tema { get; set; }
+        public int NumeroPessoas { get; set; }
+        public string Lote { get; set; }
+        public string ImagemURL { get; set; }
+    }
+}
